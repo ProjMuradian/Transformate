82712ac299baf7bce098272015a2e03e3834694d
mozaddedalfeshani <murad15-6047@diu.edu.bd>
2025-08-11 13:50:56 +0600

    Update site URLs to point to the new domain; add caniuse-lite dependency; update next-sitemap and related configurations.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://muradianconvertor.com',
+  siteUrl: process.env.SITE_URL || 'https://transformate.vercel.app',
   generateRobotsTxt: true,
   generateIndexSitemap: true,
   exclude: ['/admin/*', '/api/*'],
@@ -13,7 +13,7 @@ module.exports = {
       },
     ],
     additionalSitemaps: [
-      'https://muradianconvertor.com/sitemap.xml',
+      'https://transformate.vercel.app/sitemap.xml',
     ],
   },
   changefreq: 'daily',
